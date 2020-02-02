050894e46b44415afc62610061fd543f981867dd
Wellington <wellington.ws59@gmail.com>
2020-02-01 23:08:01 -0300

    criado atributos de instnâcia nome e idade

--- Oo/pessoa.py
+++ Oo/pessoa.py
@@ -1,8 +1,16 @@
 class  Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = None
+
     def cumprimento(self):
         return f'Olá {id(self)}'
     if __name__ == ' __main__ ':
-               p = pessoa()
+               p = pessoa('Luciano')
                print (pessoa.cumprimento(p))
                print (id(p))
                print (p.cumprimentar())
+               print (p.nome)
+               p.nome = 'Renzo'
+               print (p.nome)
+               print (p.idade)
