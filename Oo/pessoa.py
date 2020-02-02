909771cb3abdbe7dd823d385527c1fd8a271dcca
Wellington <wellington.ws59@gmail.com>
2020-02-01 22:44:27 -0300

    criado metodo cumprimentar

--- Oo/pessoa.py
+++ Oo/pessoa.py
@@ -1,2 +1,8 @@
-Class  Pessoa:
-    pass
+class  Pessoa:
+    def cumprimento(self):
+        return f'Olá {id(self)}'
+    if __name__ == ' __main__ ':
+               p = pessoa()
+               print (pessoa.cumprimento(p))
+               print (id(p))
+               print (p.cumprimentar())
